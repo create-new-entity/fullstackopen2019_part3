fe51ec0eb18e022bf6dd440c3a172c081bccac4a
Pavel <imran.pavel@gmail.com>
2019-12-02 03:12:43 +0200

    3.11

---
+++ build/precache-manifest.33c54183b44121047f3162d61ed8274f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f1b55a2a1a5ff101160716eff293e2a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0ec350e25c2d177002cf",
+    "url": "/static/css/main.2fdc35f5.chunk.css"
+  },
+  {
+    "revision": "405359535dc40613466f",
+    "url": "/static/js/2.c5dcdd58.chunk.js"
+  },
+  {
+    "revision": "0ec350e25c2d177002cf",
+    "url": "/static/js/main.6584f08b.chunk.js"
+  },
+  {
+    "revision": "df99deec2933845bfe71",
+    "url": "/static/js/runtime-main.b4276010.js"
+  }
+]);
